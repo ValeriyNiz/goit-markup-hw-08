784029c6c0f6797fb1fce4a00513357ded551086
Valeriy Nizovtsev <nizucer123@gmail.com>
2022-05-28 15:14:39 +0300

    дз8

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('[menu-open-btn]'),
-    closeMenuBtn: document.querySelector('[menu-close-btn]'),
-    menu: document.querySelector('[mob-menu]'),
+    openMenuBtn: document.querySelector('.menu-open-btn'),
+    closeMenuBtn: document.querySelector('.menu-close-btn'),
+    menu: document.querySelector('.mob-menu'),
     body: document.querySelector('body'),
   };
 
